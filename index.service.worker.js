3d50bfe03295b63ea25d38687754f31a658dffd6
fungames86 <fungames86@users.noreply.github.com>
2026-09-08 15:11:44 -0600

    Fix stick invert, keep hands on screen, camera+pads

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788900285|3108292';
+const CACHE_VERSION = '1788901881|3535799';
 /** @type {string} */
 const CACHE_PREFIX = 'Knucklebound-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
